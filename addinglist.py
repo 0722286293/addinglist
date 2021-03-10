529ffd208cc337b3c33de4b5b105d6841364a820
risika <64900522+0722286293@users.noreply.github.com>
2021-03-11 02:27:56 +0300

    first commit

---
+++ addinglist.py
@@ -0,0 +1,24 @@
+def my_function(food):
+  for x in food:
+    print(x)
+fruits=["apple","banana","cherry"]
+my_function(fruits)
+def my_function(subject):
+  for x in subject:
+    print(x)
+course=["kiswahili","maths","english"]    
+my_function(course)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
